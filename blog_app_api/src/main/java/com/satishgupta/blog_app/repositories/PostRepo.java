801067fe7172a85b7c16ce1cf303cbdda50fb8cc
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-08-08 12:58:33 +0530

    Search by title
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- blog_app_api/src/main/java/com/satishgupta/blog_app/repositories/PostRepo.java
+++ blog_app_api/src/main/java/com/satishgupta/blog_app/repositories/PostRepo.java
@@ -5,14 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.satishgupta.blog_app.entities.Category;
 import com.satishgupta.blog_app.entities.Post;
 import com.satishgupta.blog_app.entities.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
 
 public interface PostRepo extends JpaRepository<Post, Integer>{
 
     List<Post> findByUser(User user);
 
     List<Post> findByCategory(Category category);
+
+    List<Post> findByTitleContaining(String title);
+
+//    @Query("select p from Post p where p.title like :key")
+//    List<Post> searchByTitle(@Param("key") String title);
     
 }
