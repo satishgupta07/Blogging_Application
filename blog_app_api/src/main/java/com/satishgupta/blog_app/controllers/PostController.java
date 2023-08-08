c468df2c935fa41a8cefcff9edd358795d6c18d6
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-08-08 13:08:40 +0530

    Good Practice to make a contants class instead of hardcoded values
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- blog_app_api/src/main/java/com/satishgupta/blog_app/controllers/PostController.java
+++ blog_app_api/src/main/java/com/satishgupta/blog_app/controllers/PostController.java
@@ -2,6 +2,7 @@ package com.satishgupta.blog_app.controllers;
 
 import java.util.List;
 
+import com.satishgupta.blog_app.config.AppConstants;
 import com.satishgupta.blog_app.payloads.ApiResponse;
 import com.satishgupta.blog_app.payloads.PostResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,10 +45,10 @@ public class PostController {
     // get all posts
     @GetMapping("/posts")
     public ResponseEntity<PostResponse> getAllPost(
-            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "5", required = false) Integer pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir ) {
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir ) {
         PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
         return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
     }
